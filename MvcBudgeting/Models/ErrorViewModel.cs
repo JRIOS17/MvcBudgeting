9f4a1e8007a17e3dc211a5fbdc253152cae17c2b
JRIOS17 <jessiearocherios70@gmail.com>
2024-08-23 18:14:07 -0400

    Adding Initial project

---
+++ MvcBudgeting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcBudgeting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
